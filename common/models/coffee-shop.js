b9a89f05c56d2fbd882eace84f881e36ef555991
Robert Todea <robert.todea@e-spres-oh.com>
2016-04-06 13:37:58 +0000

    Enables the client to server static content

--- common/models/coffee-shop.js
+++ common/models/coffee-shop.js
@@ -1,3 +1,50 @@
 module.exports = function(CoffeeShop) {
+  registerStatus();
+  registerGetName();
 
+  function registerMethod(methodName, methodFunction, remoteSpec) {
+    CoffeeShop[methodName] = methodFunction;
+    CoffeeShop.remoteMethod(methodName, remoteSpec);
+  }
+
+  function registerStatus() {
+    var methodName = 'status';
+    registerMethod(methodName, getStatus, {
+      http: {path: '/' + methodName, verb: 'get'},
+      returns: {arg: methodName, type: 'string'}});
+  }
+
+  var OPEN_HOUR = 6;
+  var OPEN_MESSAGE = 'We are open for business';
+  var CLOSE_HOUR = 20;
+  var CLOSE_MESSAGE = 'Sorry, we are closed. We are open daily from: ' + OPEN_HOUR + ' to ' + CLOSE_HOUR;
+
+  function createStatusMessage() {
+    var currentHour = (new Date()).getHours();
+    if (OPEN_HOUR < currentHour < CLOSE_HOUR) {
+      return OPEN_MESSAGE;
+    }
+    return CLOSE_MESSAGE;
+  }
+
+  function getStatus(callback) {
+    var statusMessage = createStatusMessage();
+    callback(null, statusMessage);
+  }
+
+  function findName(shopId, callback) {
+    CoffeeShop.findById(shopId, function(err, instance) {
+      var name = 'Name of CoffeeShop is: ' + instance.name;
+      callback(null, name);
+      console.log(name);
+    });
+  }
+
+  function registerGetName() {
+    var methodName = 'getname';
+    registerMethod(methodName, findName, {
+      http: {path: '/' + methodName, verb: 'get'},
+      accepts: {arg: 'id', type: 'number', http: { source: 'query'}},
+      returns: {arg: 'name', type: 'string'}});
+  }
 };
---
+++ server/boot/routes.js
@@ -0,0 +1,3 @@
+/**
+ * Created by robert on 06/04/16.
+ */
