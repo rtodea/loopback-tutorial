3f029d6d5cbb40d00940ca9104d5d2ac9bf3bdc6
Robert Todea <robert.todea@e-spres-oh.com>
2016-04-08 11:22:28 +0000

    Remote Hook

--- common/models/review.js
+++ common/models/review.js
@@ -1,3 +1,11 @@
 module.exports = function(Review) {
-
+  Review.beforeRemote('create', beforeRemoteCreateHook);
 };
+
+function beforeRemoteCreateHook(context, user, next) {
+  var req = context.req;
+  console.log(user);
+  req.body.date = Date.now();
+  req.body.publisherId = req.accessToken.userId;
+  next();
+}
